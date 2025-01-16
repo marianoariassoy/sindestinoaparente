5777383b46464421ec87250d8a5cc86c321b5a6b
marianoariassoy <hola@marianoarias.soy>
2025-01-16 18:49:19 -0300

    add new images

--- src/components/Labels.tsx
+++ src/components/Labels.tsx
@@ -1,13 +1,12 @@
 import LabelsItems from './LabelsItems'
 
-const Labels = ({ labels, padding }) => {
+const Labels = ({ labels }) => {
   return (
     <div className='absolute z-20 px-3 top-0 pt-20 flex flex-col gap-y-6'>
       {labels.map((item, index) => (
         <LabelsItems
           key={index}
           text={item.description}
-          padding={padding[index]}
         />
       ))}
     </div>
--- src/components/LabelsMain.tsx
+++ src/components/LabelsMain.tsx
@@ -1,23 +1,11 @@
 import LabelsItems from './LabelsItems'
 
 const LabelsMain = ({ labels }) => {
-  function generateRandomPadding(count: number) {
-    return Array.from({ length: count }, () => ({
-      left: Math.round(Math.random() * 80),
-      right: Math.round(Math.random() * 80),
-      top: Math.round(Math.random() * 80),
-      bottom: Math.round(Math.random() * 50)
-    }))
-  }
-
-  const padding = generateRandomPadding(labels.length)
-
   return (
     <div className='absolute w-full z-50'>
       {labels.map((item, index) => (
         <LabelsItems
           key={index}
-          padding={padding[index]}
           text={item.description}
         />
       ))}
